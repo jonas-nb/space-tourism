f4325286f213db7e6d50d975bcd9a232dd785a4d
Jonas Batista <jonas.nb4@gmail.com>
2022-12-21 16:59:23 -0300

    feito uma rota para os icones do menu

--- src/components/Home/Menu.jsx
+++ src/components/Home/Menu.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { Link } from 'react-router-dom'
-
+import iconMenu from './assets/menu.svg'
+import iconClose from './assets/close.svg'
 const Menu_Container = styled.div``
 const Menu_Animation = keyframes`
 0% {
@@ -49,26 +50,9 @@ const Menu = () => {
                             }
                         >
                             {buttonState === false ? (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="21"
-                                >
-                                    <g fill="#D0D6F9" fill-rule="evenodd">
-                                        <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
-                                    </g>
-                                </svg>
+                                <img src={iconMenu} alt="" />
                             ) : (
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="21"
-                                >
-                                    <g fill="#D0D6F9" fill-rule="evenodd">
-                                        <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
-                                        <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
-                                    </g>
-                                </svg>
+                                <img src={iconClose} alt="" />
                             )}
                         </button>
                     </Menu_Container>
